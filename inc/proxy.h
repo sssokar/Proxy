66b29c2a3fc4f4386da9bed465b4cf6306f810b5
ssokar <deussokar@gmail.com>
2020-04-13 12:17:49 +0200

    Adding security.

--- inc/proxy.h
+++ inc/proxy.h
@@ -4,4 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <fcntl.h>
+
+typedef struct 		s_info_process
+{
+	uint32_t	pid;
+	char		**args_files;
+
+}			t_info_process;
+
+typedef struct		s_cmds
+{
+	const char	**argv;
+}			t_cmds;
+
+char	**split(char *, uint8_t);
+char	**execute_cmds(uint16_t, t_cmds *);
+long	execute_simple_cmds(t_cmds *);
 #endif
--- src/proxy.c
+++ src/proxy.c
@@ -2,19 +2,47 @@
 
 t_info_process	info_process = {0};
 
-void	handler_signal(int signal)
+// ---------------------- //
+// -- [START] SECURITY -- //
+// ---------------------- //
+static void	handler_signal(int signal)
 {
 	switch(signal)
 	{
 		case SIGTERM:
-			fprintf(stderr, "ALERT SECURITY\n");
+			// -- Delete Rootkit -- //
+			// -------------------- //
+			// -------------------- //
+			// -------------------- //
+
+			fprintf(stderr, "*** ALERT SECURITY ***\n");
+			// -- Delete info process -- //
+			//for(uint8_t i = 0; info_process.args_files[i]; i++)
+			//	remove(info_process.args_files[i]);
+
+			kill(info_process.pid, SIGKILL);
 			break;
 	}
 }
+// -------------------- //
+// -- [END] SECURITY -- //
+// -------------------- //
+
+
 
 int	main(int __attribute__((unused))argc, char ** __attribute__((unused))argv)
 {
+
+	sleep(15);
+
 	signal(SIGTERM, handler_signal);
+
+	if (argv[1] == NULL) {
+		fprintf(stderr, "Bad parameter\t\t[NOK]");
+		exit(EXIT_FAILURE);
+	}
+
+	char	*key = argv[1];
 
 	info_process.pid = getpid();
 
@@ -23,11 +51,12 @@ int	main(int __attribute__((unused))argc, char ** __attribute__((unused))argv)
 		exit(1);
 
 	sprintf(str, "/proc/%d/maps", info_process.pid);
-	const char *cmd_cat[] 	= { "/bin/cat", str, 0 };
-  	const char *cmd_awk[] 	= { "/usr/bin/awk", "{print $6}", 0 };
+
+	const char *cmd_cat[] 	= { "/bin/cat", str, key, 0 };
+  	const char *cmd_awk[] 	= { "/usr/bin/awk", "{print $6}", key, 0 };
   	t_cmds cmd[] = { {cmd_cat}, {cmd_awk}};
 
 	info_process.args_files = execute_cmds(2, cmd);
-	
+	while(1);
 	return (0);
 }
